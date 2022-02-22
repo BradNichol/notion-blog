dcf8786b79302ba15b06806fc89e7e9a927b20a2
bradleynichol <bradleynichol@live.co.uk>
2022-02-22 22:50:13 +0000

    homepage update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,23 +8,27 @@ export default function Index() {
     <>
       <Header titlePre="Home" />
       <div className={sharedStyles.layout}>
-        <h1>Hi, I'm Bradley Nichol 👋 </h1>
+        <h1 style={{ color: 'blue' }}>Hi, I'm Bradley Nichol 👋</h1>
         <h2>Back-end Software Developer</h2>
 
         <div className="explanation">
           <p>
-            I'm a software developer based in Leyland, Lancashire, working for a
-            FTSE100 company in the automotive industry. I predominately work on
+            I'm a software developer based in Manchester, UK, working for a FTSE
+            100 company in the automotive industry. I predominately work on
             backend systems using Java, with a sprinkle of Angular frontend work
             for internal tools.
           </p>
-
+          <p>
+            I have also had exposure to tools such as DBT, Airflow and BigQuery
+            whilst developing data products for internal customer use.
+          </p>
           <p>
             In my past life I was a digital marketing professional, and after
-            two years of self-development I transitioned into a career in
-            software development. I'm sharing my thoughts, journey and what I'm
-            learning here. Thanks for stopping by.
+            two years of self-learning I transitioned into a career in software
+            development. I'm sharing my development thoughts and what I'm
+            learning here.
           </p>
+          <p>Thanks for stopping by.</p>
         </div>
       </div>
     </>
