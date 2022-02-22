e26c00581d1422419a75ff3e82ccda46f43ec50e
bradleynichol <bradleynichol@live.co.uk>
2022-02-22 23:01:23 +0000

    change to blog title

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -63,7 +63,10 @@ const Index = ({ posts = [], preview }) => {
         </div>
       )}
       <div className={`${sharedStyles.layout} ${blogStyles.blogIndex}`}>
-        <h1>My Notion Blog</h1>
+        <h3>
+          “The beautiful thing about learning is nobody can take it away from
+          you.” - B.B. King
+        </h3>
         {posts.length === 0 && (
           <p className={blogStyles.noPosts}>There are no posts yet</p>
         )}
