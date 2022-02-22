2d8e17ce652fbbe08438244cb578b50953de20b1
bradleynichol <bradleynichol@live.co.uk>
2022-02-22 20:14:35 +0000

    further additions

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,25 +8,8 @@ export default function Index() {
     <>
       <Header titlePre="Home" />
       <div className={sharedStyles.layout}>
-        <img
-          src="/vercel-and-notion.png"
-          height="85"
-          width="250"
-          alt="Vercel + Notion"
-        />
         <h1>Hi, I'm Bradley Nichol 👋 </h1>
-        <h2>
-          Blazing Fast Notion Blog with Next.js'{' '}
-          <ExtLink
-            href="https://github.com/vercel/next.js/issues/9524"
-            className="dotted"
-            style={{ color: 'inherit' }}
-          >
-            SSG
-          </ExtLink>
-        </h2>
-
-        <Features />
+        <h2>Back-end Software Developer</h2>
 
         <div className="explanation">
           <p>
